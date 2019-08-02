2b12674e8cdb25fe51c2c2530652cadc3a5f7619
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2019-08-02 11:21:09 +0200

    Finish configuration

--- src/DependencyInjection/WebtownWorkflowExtension.php
+++ src/DependencyInjection/WebtownWorkflowExtension.php
@@ -9,7 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class WorkflowExtension extends Extension
+class WebtownWorkflowExtension extends Extension
 {
 
     /**
